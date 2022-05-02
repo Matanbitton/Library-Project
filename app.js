31903f0fe77ddb3d41f108d9c28228094656a40d
Matan <matanbitton2@gmail.com>
2022-05-02 16:27:41 +0300

    Refractored the code to use classes

--- app.js
+++ app.js
@@ -1,139 +1,130 @@
-const myLibrary = [];
-let booksCount = -1;
-
-function book(title,author,numPages,read){
-    this.title = title
-    this.author = author
-    this.numPages = numPages
+class Book {
+  constructor(title, author, numPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
     this.read = read;
+  }
 }
+class Library {
+  constructor() {
+    this.books = [];
+  }
+  addBookToLibrary = (book) => {
+    this.books.push(book);
+  };
+  getBook(bookName) {
+    return this.books.find((book) => book.title == bookName);
+  }
+  removeFromLibrary(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  }
+}
+const library = new Library();
 
-book.prototype.addBookToLibrary = function(){
-    myLibrary.push(this);
-}
-// this function creates the card component 
+// this function creates the card component
 
 function createBookCard(book) {
-    booksCount++;
+  const card = document.createElement("div");
+  card.className = "card";
+  card.dataset.title = book.title;
 
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.dataset.numPlaceInLibrary = booksCount;
+  const cardBookNameSection = document.createElement("p");
+  cardBookNameSection.className = "card-book-name";
+  cardBookNameSection.innerText = `Book Name: ${book.title}`;
 
-    const cardBookNameSection = document.createElement('p')
-    cardBookNameSection.className = 'card-book-name';
-    cardBookNameSection.innerText = `Book Name: ${book.title}`;
+  const cardAuthorNameSection = document.createElement("p");
+  cardAuthorNameSection.className = "card-author-name";
+  cardAuthorNameSection.innerText = `Author: ${book.author}`;
 
-    const cardAuthorNameSection = document.createElement('p')
-    cardAuthorNameSection.className = 'card-author-name';
-    cardAuthorNameSection.innerText = `Author: ${book.author}` ;
+  const cardNumOfPages = document.createElement("p");
+  cardNumOfPages.className = "card-num-of-pages";
+  cardNumOfPages.innerText = `Pages: ${book.numPages}`;
 
-    const cardNumOfPages = document.createElement('p');
-    cardNumOfPages.className = 'card-num-of-pages';
-    cardNumOfPages.innerText =`Pages: ${book.numPages}`;
+  const readButton = document.createElement("button");
+  readButton.className = "read-button";
+  readButtonState(book, readButton);
 
-    const readButton = document.createElement('button')
-    readButton.className = 'read-button';
-    console.log(book.read);
-    readButtonState(book,readButton);
+  const removeButton = document.createElement("button");
+  removeButton.className = "remove";
+  removeButton.innerText = "Remove";
+  removeButtonFunctionality(card, removeButton);
 
-
-    const removeButton = document.createElement('button')
-    removeButton.className = 'remove';
-    removeButton.innerText = 'Remove';
-    removeButtonFunctionality(card,removeButton);
-
-    card.append(cardBookNameSection)
-    card.append(cardAuthorNameSection)
-    card.append(cardNumOfPages)
-    card.append(readButton)
-    card.append(removeButton)
-    booksContainer.append(card)
+  card.append(cardBookNameSection);
+  card.append(cardAuthorNameSection);
+  card.append(cardNumOfPages);
+  card.append(readButton);
+  card.append(removeButton);
+  booksContainer.append(card);
 }
 
-function readButtonState(book,button) {
-    if (book.read) {
-        button.innerText = "Read";
-        button.style.backgroundColor = "rgb(49, 206, 49)";
-      
-    }else {
-        button.innerText = "Didn't Read";
-        button.style.backgroundColor = "rgb(218, 79, 79)";
-      
+function readButtonState(book, button) {
+  if (book.read) {
+    button.innerText = "Read";
+    button.style.backgroundColor = "rgb(49, 206, 49)";
+  } else {
+    button.innerText = "Didn't Read";
+    button.style.backgroundColor = "rgb(218, 79, 79)";
+  }
+  button.addEventListener("click", () => {
+    console.log("clicked");
+    if (button.innerText == "Read") {
+      book.read = false;
+      button.innerText = "Didn't Read";
+      button.style.backgroundColor = "rgb(218, 79, 79)";
+    } else {
+      button.innerText = "Read";
+      book.read = true;
+      button.style.backgroundColor = "rgb(49, 206, 49)";
     }
-    button.addEventListener("click", () => {
-        
-
-        if(button.innerText == "Read") {
-            book.read = true;
-            button.innerText = "Didn't Read";
-            button.style.backgroundColor = "rgb(218, 79, 79)";
-        } else{
-           button.innerText = "Read";
-           book.read = false;
-           button.style.backgroundColor = "rgb(49, 206, 49)";
-           
-        }
-     
-    });
-        
+  });
 }
 
-function removeButtonFunctionality(card,button){
-    button.addEventListener("click", () => {
-        card.remove();
-        console.log(card.dataset.numPlaceInLibrary);
-        removeFromLibrary(card.dataset.numPlaceInLibrary);
-        
-     })
+function removeButtonFunctionality(card, button) {
+  button.addEventListener("click", () => {
+    card.remove();
+    library.removeFromLibrary(card.dataset.title);
+  });
 }
 
-
-const popUp = document.querySelector('.form-popup');
-const modalLayover = document.querySelector('.modal-background');
-const booksContainer = document.querySelector('.books-container');
-const wrapper = document.querySelector('.wrapper');
+const popUp = document.querySelector(".form-popup");
+const modalLayover = document.querySelector(".modal-background");
+const booksContainer = document.querySelector(".books-container");
+const form = document.getElementById("book-form");
 
 hideForm(popUp);
 hideForm(modalLayover);
 
-const form = document.getElementById('book-form');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-form.addEventListener('submit', (event) =>  {
-    event.preventDefault();
-    
-    const bookName = document.getElementById('book-name').value;
-    const bookAuthor = document.getElementById('author-name').value;
-    const bookNumPages = document.getElementById('number-of-pages').value;
-    let read = document.getElementById('read-validation');
-    if(read.checked) {
-        read = true;
-    }else {
-        read = false;
-    }
-    const newBook = new book(bookName, bookAuthor, bookNumPages, read);
-    createBookCard(newBook);
-    newBook.addBookToLibrary();
-    hideForm(popUp);
-    hideForm(modalLayover);
+  const bookName = document.getElementById("book-name").value;
+  const bookAuthor = document.getElementById("author-name").value;
+  const bookNumPages = document.getElementById("number-of-pages").value;
+  let read = document.getElementById("read-validation");
+  if (read.checked) {
+    read = true;
+  } else {
+    read = false;
+  }
+  const newBook = new Book(bookName, bookAuthor, bookNumPages, read);
+
+  createBookCard(newBook);
+  library.addBookToLibrary(newBook);
+  hideForm(popUp);
+  hideForm(modalLayover);
 });
-    
 
 function showForm(element) {
-    element.style.display = 'block';
+  element.style.display = "block";
 }
-function hideForm(element){
-    element.style.display = 'none';
+function hideForm(element) {
+  element.style.display = "none";
 }
 
-const addBookButton = document.querySelector('.new-book');
-addBookButton.addEventListener("click", () => { 
-    form.reset();
-    showForm(popUp);
-    showForm(modalLayover);
+const addBookButton = document.querySelector(".new-book");
+addBookButton.addEventListener("click", () => {
+  form.reset();
+  showForm(popUp);
+  showForm(modalLayover);
 });
-
-
-function removeFromLibrary(bookPlaceInArray) {
-     delete(myLibrary[bookPlaceInArray]);
-}   
